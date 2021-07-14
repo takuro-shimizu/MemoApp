062855d18ff88837af29d9711b8e9d8446f7f8a3
Takuro Shimizu <72232150+takuro-shimizu@users.noreply.github.com>
2021-07-14 18:56:33 +0900

    Touchable

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View, Text, StyleSheet, TouchableOpacity,
+} from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 export default function MemoList() {
@@ -10,27 +12,33 @@ export default function MemoList() {
                     <Text style={styles.memoListItemTitle}>買い物リスト</Text>
                     <Text style={styles.memoListItemDate}>2021年7月13日 10:00</Text>
                 </View>
+                <TouchableOpacity>
                 <View>
                     <Feather name="x" size={16} color="#B0B0B0"/>
                 </View>
+                </TouchableOpacity>
             </View>
             <View style={styles.memoListItem}>
                 <View>
                     <Text style={styles.memoListItemTitle}>買い物リスト</Text>
                     <Text style={styles.memoListItemDate}>2021年7月13日 10:00</Text>
                 </View>
+                <TouchableOpacity>
                 <View>
                 <Feather name="x" size={16} color="#B0B0B0"/>
                 </View>
+                </TouchableOpacity>
             </View>
             <View style={styles.memoListItem}>
                 <View>
                     <Text style={styles.memoListItemTitle}>買い物リスト</Text>
                     <Text style={styles.memoListItemDate}>2021年7月13日 10:00</Text>
                 </View>
+                <TouchableOpacity>
                 <View>
                 <Feather name="x" size={16} color="#B0B0B0"/>
                 </View>
+                </TouchableOpacity>
             </View>
         </View>
     );
